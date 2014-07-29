87bee22e55c55b3cc68a4da9fcdf0af393ca6bfe
Cristian Sulea <cristian.sulea.79@gmail.com>
2014-07-29 16:15:01 +0300

    cleanup

--- jatoo-exec-remote/src/main/java/jatoo/exec/remote/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * A free open source Java library created to ease the work with remote processes execution.
- * 
- * @author <a href="http://cristian.sulea.net" rel="author">Cristian Sulea</a>
- */
-package jatoo.exec.remote;
-
